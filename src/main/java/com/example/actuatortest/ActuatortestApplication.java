fcd251c0db51a00978b468adac577bb1fe232915
machida tetsuro <town-field@live.jp>
2020-08-25 17:08:39 +0900

    Initial commit

---
+++ src/main/java/com/example/actuatortest/ActuatortestApplication.java
@@ -0,0 +1,13 @@
+package com.example.actuatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatortestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatortestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actuatortest/ActuatortestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatortestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
